2258f72220c803d0f4ab481b2ba006fcd9b92a49
Yiyang Lin <davidlinyiyang@gmail.com>
2021-11-10 22:33:21 -0600

    title finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+          <path d="M0,260 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{fill: "#f3e0cc", zIndex: -1}}></path>
+          
+        </svg>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hi! I am Yiyang Lin. 
+          Hi! I am Yiyang Lin
         </h1>
         <h2>
-          You can just call me David.
+          You can just call me David
         </h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          I am a MCS at UIUC
+        </h2>
       </header>
+            
     </div>
   );
 }
