7636d06072a9094ab4436e0fb7c4be3ca961cfce
Yiyang Lin <davidlinyiyang@gmail.com>
2021-11-10 23:11:19 -0600

    improve title

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,30 @@ function App() {
           <path d="M0,260 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{fill: "#f3e0cc", zIndex: -1}}></path>
           
         </svg>
+        <header className="App-header">
+          <h1>
+            Hi! I am Yiyang Lin
+          </h1>
+          <h2>
+            You can just call me David
+          </h2>
+          <h2>
+            I am a MCS at UIUC
+          </h2>
+        </header>
       </div>
-      <header className="App-header">
-        <h1>
-          Hi! I am Yiyang Lin
+      {Array.from({length: 25}, (_, index) => index + 1).map((i) => {
+        return <header className="App-header" style={{opacity: 0.2, fontSize: `${i}vw`}}>
+        <h1 style={{opacity: 0.2, fontSize: `${0.5*i}vw`}}>
+          Hi! 
         </h1>
-        <h2>
-          You can just call me David
-        </h2>
-        <h2>
-          I am a MCS at UIUC
-        </h2>
+       
       </header>
+      })}
             
     </div>
+
+    
   );
 }
 
