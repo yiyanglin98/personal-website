deef675e94252efade75a000bd706b98df1bf1c1
Yiyang Lin <davidlinyiyang@gmail.com>
2021-11-15 22:17:34 -0600

    fix alignment

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,16 @@ function App() {
           
         </svg>
         <header>
-        {Array.from({length: 18}, (_, index) => index + 1).map((i) => {
+        {/* {Array.from({length: 18}, (_, index) => index + 1).map((i) => {
         return <header className="App-header" style={{opacity: 0.1, position: "absolute"}}>
         <h1 style={{fontSize: `${0.5*i}vw`}}>
           Hi! 
         </h1>
        
       </header>
-      })}
+      })} */}
           <h1>
-            Hi! <br/><br/>I am Yiyang Lin
+            Hi! <br/>I am Yiyang Lin
           </h1>
           <h2>
             You can just call me David
@@ -37,11 +37,14 @@ function App() {
       <div className="projects-container" style={{backgroundColor:"#4a4844"}}>
         <svg viewBox="0 0 500 300" preserveAspectRatio="xMinYMin meet" className="game-svg" style={{ zIndex:-1}}>
             <path d="M0,260 C50,200 100,0 500,100 L500,00 L0,0 Z" style={{fill: "#4a4844"}}></path>
-
         </svg>
-        <DecoText title display>My Game Project</DecoText> 
-        <p style={{margin: "0 0 30px 40px", color: "#e1b47c"}}>move cursor on any video to see more</p>
+
         <div style={{width: "50%", margin: "0 auto"}}>
+          <DecoText title display>My Game Project</DecoText> 
+          <p style={{margin: "0 0 30px 40px", color: "#e1b47c"}}>move cursor on any video to see more</p>
+        </div>
+        
+        <div style={{width: "50%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", margin: "0 auto"}}>
           <GameCard 
               link="http://publish.illinois.edu/david-lin/files/2020/08/Screen-Recording-2020-08-02-at-11.47.00-PM.mp4" 
               text="fighter aircraft simulator with shooting, created with Unity"/>
--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -8,7 +8,7 @@ function GameCard({link, text}:{link:string, text:string}) {
     return (
         <div>
             <div style={{display: hover ? "inherit" : "none", position: "absolute", fontStyle: "italic"}}><DecoText title={false} display>{text}</DecoText></div>
-            <video height="auto" width="500" autoPlay muted loop style={{margin: "25px 100px 25px 100px", opacity: hover ? 0.2 : 1}} onMouseEnter={()=> {setHover(true)}} onMouseLeave={()=> {setHover(false)}}>
+            <video height="auto" width="520" autoPlay muted loop style={{borderRadius: "20px", margin: "25px 100px 25px 100px", opacity: hover ? 0.2 : 1}} onMouseEnter={()=> {setHover(true)}} onMouseLeave={()=> {setHover(false)}}>
                 <source src={link} type="video/mp4" />
             </video>
         </div>
