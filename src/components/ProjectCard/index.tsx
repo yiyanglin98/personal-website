787b7c5748a76d22cab7bc95f0079fbd3cb17d98
Yiyang Lin <davidlinyiyang@gmail.com>
2021-11-16 18:33:41 -0600

    filter

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -7,7 +7,7 @@ function ProjectCard({link, text, title}:{link:string, text:string, title: strin
         <div className="flex flex-col ">
             <DecoText title display>{title}</DecoText>
             <DecoText title={false} display>{text}</DecoText>
-            <img className="rounded w-1/2 m-20 filter grayscale blur-sm contrast-200 hover:filter-none" src={link} alt={title} ></img>
+            <img className="rounded w-1/2 m-20 filter grayscale blur-sm hover:filter-none" src={link} alt={title} ></img>
         </div>
     )
 }
